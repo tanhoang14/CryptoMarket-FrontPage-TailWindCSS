cd97e7be08cc5398c9fec573538090170465e0d3
Duc Tan Hoang <ductan.hoang@sjsu.edu>
2022-07-10 23:19:39 -0700

    first commit

---
+++ src/components/Content/Content.jsx
@@ -0,0 +1,54 @@
+import {FaBitcoin} from "react-icons/fa"
+import { useSpring,animated } from "react-spring"
+import {FaEthereum} from "react-icons/fa"
+import {SiLitecoin} from "react-icons/si"
+import checkIconMobile from "../../assets//mobile/checkpoint.svg"
+const Content = () => {
+    // const customer = useSpring({ customers: 10245, from: { customers: 0 } });
+    return ( 
+        <section className="md:flex md:flex-row">
+            <div className="ml-[70px] mr-[54px] text-bold flex flex-col items-center bg-gradient-to-b shadow-md from-white/40 rounded-[24px] to transparent md:w-[25%]">
+                <div className="mt-16 gap-8 flex items-start">
+                    <FaBitcoin size={"50px"} className="mt-[12px]"/>
+                        <div className="text-26">
+                            20245
+                            {/* <animated.div>
+                                {customer.customers.to((val)=>Math.floor(val))}
+                            </animated.div> */}
+                            <p className="text-13 font-semibold mb-[36px] md:text-16">Bitcoins</p>
+                        </div> 
+                </div>
+                <div className="flex gap-8 items-start mb-[40px]">
+                    <FaEthereum size={"50px"} className="mt-[12px]"/>
+                    <div className="text-26">
+                        12045
+                        <p className="text-13 font-semibold md:text-16"> Ethereums </p>
+                    </div>
+                </div>
+                <div className="flex gap-8 items-start mb-16">
+                    <SiLitecoin size={"50px"} className="mt-[12px]"/>
+                    <div className="text-26">
+                        1455
+                        <p className="text-13 font-semibold md:text-16"> LiteCoins </p>
+                    </div>
+                </div>
+            </div>
+            <div className="text-13 flex-col w-[100%] mt-[90px] font-semibold md:mt-0 md:w-[30%] md:text-16 md:ml-[20%]">
+                <div className="mb-[51px] flex gap-8 ml-[70px] mr-[29px] items-center md:mx-0">
+                    <img src={checkIconMobile} alt=""/>
+                    <p> Create your free account</p>
+                </div>
+                <div className="mb-[51px] flex gap-8 ml-[70px] mr-[29px] items-center md:mx-0">
+                    <img src={checkIconMobile} alt=""/>
+                    <p> Connect your funding method</p>
+                </div>
+                <div className="mb-[51px] flex gap-8 ml-[70px] mr-[29px] items-center md:mx-0">
+                    <img src={checkIconMobile} alt=""/>
+                    <p> Buy and sell 100+ cryptocurrencies</p>
+                </div>
+            </div>
+        </section>
+     );
+}
+ 
+export default Content;
---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,55 @@
+import { useState } from 'react';
+import { RiCopperCoinLine } from 'react-icons/ri';
+import {HiOutlineMenu} from 'react-icons/hi'
+import {MdOutlineClose} from 'react-icons/md'
+import {FaRegMoon} from 'react-icons/fa'
+import useDarkMode from '../../useDarkMode';
+
+const NavBar = (props) => {
+    const {isMobile}=props
+    const [openMenu, setOpenMenu] = useState(false);
+    const [isDarkMode, toggleDarkMode] = useDarkMode()
+    const handleMenu = () =>{
+        setOpenMenu(!openMenu)
+    }
+    return (  
+        <nav className="flex item-center">
+            <div className="flex item-center">
+                <div className="text-20 font-bold mr-2">CrytoPeople</div>
+                {isDarkMode ? (
+                <RiCopperCoinLine size={"24px"} color={"#e9c46a"} className="cursor-pointer" onClick={()=>toggleDarkMode(!isDarkMode)}/>
+
+                ):(
+                <FaRegMoon size={"24px"} color={"#e9c46a"} className="cursor-pointer" onClick={()=>toggleDarkMode(!isDarkMode)}/>
+
+                )}   
+            </div>
+            <ul className="md:flex md:gap-10 ml-auto text-16 font-semibold">
+                {openMenu && isMobile ? (
+                    <MdOutlineClose size={"24px"} className="cursor-pointer" onClick={handleMenu}/>
+                ):!openMenu && isMobile ?(
+                    <HiOutlineMenu size={"24px"} className="cursor-pointer" onClick={handleMenu}/>
+
+                ):(
+                    <>
+                        <li className="hover:text-purple-500 transition-all duration-200 cursor-pointer">Features</li>
+                        <li className="hover:text-purple-500 transition-all duration-200 cursor-pointer">Menu</li>
+                        <li className="hover:text-purple-500 transition-all duration-200 cursor-pointer">NFT</li>
+                        <li className="hover:text-purple-500 transition-all duration-200 cursor-pointer">Contact</li>
+                    </>
+                )}
+                {openMenu && (
+                    <div className='absolute right-8 bg-white p-8 text-center z-10 text-black text-13'>
+                        <li className="cursor-pointer">Features</li>
+                        <li className="cursor-pointer">Menu</li>
+                        <li className="cursor-pointer">NFT</li>
+                        <li className="cursor-pointer">Contact</li>
+
+                    </div>
+                )}
+            </ul>
+        </nav>
+    );
+}
+ 
+export default NavBar;
