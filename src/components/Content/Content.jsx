981e8ced9397e3580cf2e625df1259ae9b1f9fbd
Duc Tan Hoang <ductan.hoang@sjsu.edu>
2022-07-10 23:32:44 -0700

    2nd commit

--- src/components/Content/Content.jsx
+++ src/components/Content/Content.jsx
@@ -1,20 +1,20 @@
 import {FaBitcoin} from "react-icons/fa"
-import { useSpring,animated } from "react-spring"
+// import { useSpring,animated } from "react-spring"
 import {FaEthereum} from "react-icons/fa"
 import {SiLitecoin} from "react-icons/si"
 import checkIconMobile from "../../assets//mobile/checkpoint.svg"
 const Content = () => {
-    const customer = useSpring({ customers: 10245, from: { customers: 0 } });
+    // const customer = useSpring({ customers: 10245, from: { customers: 0 } });
     return ( 
         <section className="md:flex md:flex-row">
             <div className="ml-[70px] mr-[54px] text-bold flex flex-col items-center bg-gradient-to-b shadow-md from-white/40 rounded-[24px] to transparent md:w-[25%]">
                 <div className="mt-16 gap-8 flex items-start">
                     <FaBitcoin size={"50px"} className="mt-[12px]"/>
                         <div className="text-26">
-                            
-                            <animated.div>
+                            10245
+                            {/* <animated.div>
                                 {customer.customers.to((val)=>Math.floor(val))}
-                            </animated.div>
+                            </animated.div> */}
                             <p className="text-13 font-semibold mb-[36px] md:text-16">Bitcoins</p>
                         </div> 
                 </div>
